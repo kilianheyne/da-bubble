20d99cbc1ce7f9720a068750a22500619faa36dc
Kilian <kilianheyne@icloud.com>
2025-09-13 12:58:55 +0200

    simple structure for chat-board

---
+++ src/app/chat-board/chat-board.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-board',
+  imports: [],
+  templateUrl: './chat-board.component.html',
+  styleUrl: './chat-board.component.scss'
+})
+export class ChatBoardComponent {
+
+}
---
+++ src/app/login-screen/login-screen.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-screen',
+  imports: [],
+  templateUrl: './login-screen.component.html',
+  styleUrl: './login-screen.component.scss'
+})
+export class LoginScreenComponent {
+
+}
