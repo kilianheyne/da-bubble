2d70f47f2b8a1da4e7d47b5170f0e4a0a0cc5fa5
Kilian <kilianheyne@icloud.com>
2025-09-14 17:34:21 +0200

    trying out lucide for easier use of icons

--- src/app/chat-board/header/header.component.ts
+++ src/app/chat-board/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ChevronDown, LucideAngularModule, SearchIcon } from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  readonly SearchIcon = SearchIcon;
+  readonly ChevronDownIcon = ChevronDown;
 }
